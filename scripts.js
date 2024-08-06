8c73be4937e086b3792ca50c677a02fc675c1193
Hugo-Rizzo-Dev <rizzo_hugo@hotmail.com>
2024-08-06 13:07:09 -0300

    att

--- scripts.js
+++ scripts.js
@@ -1,1 +1,24 @@
-testeeeee
+//Get the page title
+console.log(document.title)
+
+//Acess the element by ID
+const guest = document.getElementById("guest-2")
+
+console.log(guest)
+
+
+//Shows properties of the object
+console.dir(guest)
+
+
+//Access element with class
+const guetsByClass = document.getElementsByClassName("guest")
+console.log(guetsByClass)
+
+//Display the first element in the list
+console.log(guetsByClass.item(0))
+console.log(guetsByClass[1])
+
+const guestsTag = document.getElementsByTagName("li")
+console.log(guestsTag)
+
